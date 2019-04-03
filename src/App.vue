c5e64cdbf9490d0cfcafe1e80612a14aef1cc13d
229394 <2293948658@qq.com>
2019-04-03 13:08:27 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h2>请对即将上映的复联4发表评论</h2>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"/>
+      <List :comments="comments" :deleteComment="deleteComment"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Add from './components/Add.vue'
+  import List from './components/List.vue'
+  export default {
+
+    data () {
+      return {
+        //数据在哪个组件,更新数据的方法就应该定义在哪个组件
+        comments: [
+          {
+            name: 'lsq',
+            content: '复联是啥?(手动滑稽)'
+          },
+          {
+            name: 'autumn_leaf',
+            content: '这部电影值得一看!'
+          },
+          {
+            name: 'taotao',
+            content: '复联大结局必去!'
+          }
+        ]
+      }
+    },
+    methods: {
+      //添加评论
+      addComment (comment) {
+        this.comments.unshift(comment)
+      },
+      //删除指定下标的评论
+      deleteComment (index) {
+        this.comments.splice(index, 1)
+      }
+    },
+    components: {
+      Add,
+      List
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复:</h3>
+    <h3 v-show="comments.length===0">暂无评论,点击左侧添加评论!</h3>
+    <ul class="list-group">
+      <Item v-for="(comment,index) in comments" :key="index" :comment="comment" :deleteComment="deleteComment" :index="index"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+    import Item from './Item.vue'
+    export default {
+      //声明接收属性,这个属性就会成为组件对象的属性
+      //此处只指定了属性名
+      props: ['comments', 'deleteComment'],
+      components: {
+        Item
+      }
+
+    }
+</script>
+
+<style>
+  .reply {
+    margin-top: 0px;
+  }
+</style>
